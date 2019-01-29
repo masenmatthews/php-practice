cb95ffc519bb735e3df3e3914b63babe48b446ae
Masen Matthews <masenmatthews@gmail.com>
2019-01-29 15:59:07 -0800

    initiate file structure, add first two exercises

---
+++ hello-world/index.php
@@ -0,0 +1,14 @@
+<?php
+    $message = "Hello World!";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>helloworld</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <h1><?php echo $message; ?></h1>
+  </body>
+</html>
